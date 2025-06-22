fdf20a455031c2eed7871ed610a8943ac6ee041e
Kishanx08 <kishanx03@proton.me>
2025-06-22 18:11:54 +0530

    added some config

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
 
 // Authentication middleware
 const authenticate = (req, res, next) => {
